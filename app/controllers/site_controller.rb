b5fd1d9f487f4f478a8bdc120505ab15b0babad3
Yang Liu <lyangpku@gmail.com>
2012-02-03 21:34:42 +0800

    add newsfeeds

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,9 +2,11 @@ class SiteController < ApplicationController
   def index
     redirect_to home_path if user_signed_in?
     @activities = Activity.last(3)
+    @events = Event.order('updated_at DESC').first(5)
   end
 
   def home
+    @newsfeeds = current_user.newsfeeds.order("id DESC")
   end
 
 end
