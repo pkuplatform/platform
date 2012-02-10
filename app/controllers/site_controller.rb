c0aebeebddfc6c85b1058500f388624a16afb283
Yang Liu <lyangpku@gmail.com>
2012-02-11 01:14:54 +0800

    v0.9

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,10 +1,10 @@
 class SiteController < ApplicationController
   before_filter :authenticate_user!, :only => [:home]
-  layout "site"
 
   def index
     redirect_to home_path if user_signed_in?
-    @activities = Activity.last(3)
+    @banners = Activity.last(3)
+    @activities = Activity.last(6)
     @events = Event.where("object_type != 'Comment'").order('updated_at DESC').first(5)
     @secondary_scope = nil
   end
