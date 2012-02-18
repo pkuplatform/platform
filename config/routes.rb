6582caa2d3f4f7aa4285d0cb721fecbfe9640ae8
Xiuhan Hu <huxiuhan@gmail.com>
2012-02-18 23:48:34 +0800

    评论样式修改

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Platform::Application.routes.draw do
 
-  devise_for :users, :controllers => { :registrations => "registrations" }
+  devise_for :users, :controllers => { :registrations => "registrations" } do
+   get 'users/sign_out' => "devise/sessions#destroy"
+  end
+
 
   get "admin/index"
 
